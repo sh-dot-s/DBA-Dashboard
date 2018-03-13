8cfa93eb7b67f0f5a502ec0257a7dc2cac68a8cc
Sankarasetty Lokesh Sai Sri Harsha <SankarsettyLokesh.SaiSriHarsha@mrcooper.com>
2018-03-13 20:49:59 +0530

    Added Material Components Web
    Signed-off-by: Sankarasetty Lokesh Sai Sri Harsha <SankarsettyLokesh.SaiSriHarsha@mrcooper.com>

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import "material-components-web/material-components-web.scss";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
